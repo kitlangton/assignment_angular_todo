07e4c3a048ee3a8619153571a14767b45ec48b8c
Kit Langton <kitlangton@gmail.com>
2016-03-25 12:49:01 -0400

    whee(

--- js/controllers/toDoController.js
+++ js/controllers/toDoController.js
@@ -17,10 +17,30 @@ toDo.controller('toDoCtrl',
 
     $scope.newDueDate = new Date();
 
+    $scope.showCompleted = true;
+    $scope.toggleStatus = "Hide Completed"
+
+    $scope.toggleComplete = function() {
+      $scope.showCompleted = !$scope.showCompleted;
+      $scope.toggleStatus = $scope.showCompleted ? "Hide Completed" : "Show Completed"
+    };
+
     $scope.createToDo = function() {
       $scope.items.push({ text: $scope.newTask, dueDate: $scope.newDueDate, completed: false});
       $scope.newTask = "";
       $scope.newDueDate = new Date();
     };
+
+    $scope.deleteTodo = function(todo) {
+      $scope.items.splice($scope.items.indexOf(todo), 1);
+    }
+
+    $scope.clearCompleted = function() {
+      $scope.items = $scope.items.filter(function(item) {
+        return !item.completed
+      })
+    }
+
+
   }
 ]);
